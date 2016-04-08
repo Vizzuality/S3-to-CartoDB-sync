97648d9a7946562b892ea32bdb48959c2175c55f
Miguel Mendoza <info@miguelmendoza.dj>
2016-04-08 09:13:05 +0200

    changes export csv query

--- app/synchronicity/data_management/exporter.rb
+++ app/synchronicity/data_management/exporter.rb
@@ -6,7 +6,7 @@ module Synchronicity
 
     def export
       path = Rails.root.join('public', 'system', 'aggregate_data.csv').to_s
-      query = "COPY value_objects TO '#{path}' DELIMITER ',' CSV HEADER;"
+      query = "\COPY value_objects TO '#{path}' DELIMITER ',' CSV HEADER;"
       ValueObject.connection.execute(query)
     end
   end
