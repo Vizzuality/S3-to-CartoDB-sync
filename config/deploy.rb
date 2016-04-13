d2c1c92e5371b314b9923759f94435666449594c
Miguel Mendoza <info@miguelmendoza.dj>
2016-04-13 14:40:01 +0200

    changes deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :scm, :git
 set :repo_url, 'git@github.com:Vizzuality/S3-to-CartoDB-sync.git'
 set :passenger_restart_with_touch, true
 
-set :rvm_type, :user
+set :rvm_type, :auto
 set :rvm_ruby_version, '2.3.0-p0@s3_to_cartodb_sync'
 set :rvm_roles, [:app, :web]
 
@@ -18,27 +18,18 @@ set :keep_releases, 5
 set :linked_files, %w{.env}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :rvm_map_bins, fetch(:rvm_map_bins, []).push('rvmsudo')
 set :foreman_use_sudo, :rvm # Set to :rbenv for rbenv sudo, :rvm for rvmsudo or true for normal sudo
 set :foreman_roles, :all
 set :foreman_template, 'upstart'
-set :foreman_export_path, ->{ File.join(Dir.home, '.init') }
+set :foreman_export_path, '/etc/init'
 set :foreman_options, ->{ {
-  app: application,
+  app: 's3_to_cartodb_sync',
   log: File.join(shared_path, 'log')
 } }
 
-# desc 'Setup Foreman'
-# task :setup_foreman do
-#   on release_roles :all do |host|
-#     execute 'rvmsudo foreman export --app s3_to_cartodb_sync --user ubuntu upstart /etc/init'
-#     execute 'foreman start'
-#   end
-# end
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
   after 'deploy:publishing', 'deploy:restart'
 end
-
-#after 'deploy:cleanup', :setup_foreman
-
