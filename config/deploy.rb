b2072aaf6718001cb6cd3ade6d0bbcf5e01f7b39
Miguel Mendoza <info@miguelmendoza.dj>
2016-04-13 14:11:48 +0200

    changes deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,9 +16,17 @@ set :deploy_to, '~/s3_to_cartodb_sync'
 set :keep_releases, 5
 
 set :linked_files, %w{.env}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{config log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
   after :finishing, 'deploy:cleanup'
   after 'deploy:publishing', 'deploy:restart'
+end
+
+before :published, :setup_foreman
+desc 'Setup Foreman'
+task :setup_foreman do
+  on release_roles :all do |host|
+    execute 'rvmsudo foreman export --app s3_to_cartodb_sync --user ubuntu upstart /etc/init'
+  end
 end
