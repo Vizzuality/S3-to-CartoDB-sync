83a95861d705ed7dd548aa2ba9dfbea42ddaf517
Miguel Mendoza <miguel.mendoza@vizzuality.com>
2016-07-14 15:51:43 +0200

    changes deploy url

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server 'ec2-52-23-207-148.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{web app db}, primary: true
+server 'nycbillboard.vizzuality.com', user: 'ubuntu', roles: %w{web app db}, primary: true
 set :ssh_options, {
   forward_agent: true
 }
